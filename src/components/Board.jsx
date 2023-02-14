6919c8a20fe04c6fcfbafeb737902fd46dbf10de
Niete <nieteis@gmail.com>
2023-02-14 16:53:02 +0000

    Tiles now showing on the page

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,22 +1,35 @@
-import React, {useState} from "react";
-// import Tile from "./Tile";
-import Cell from  "./Cell";
-import { Board } from "../helper/index.js"
+import React, { useState } from "react";
+import Tile from "./Tile";
+import Cell from "./Cell";
+import { Board } from "../helper/index.js";
 
 const BoardView = () => {
   const [board, setBoard] = useState(new Board());
 
   const cells = board.cells.map((row, rowIndex) => {
+    return (
+      <div>
+        {row.map((col, colIndex) => {
+          return <Cell />;
+        })}
+      </div>
+    );
+  });
+
+  const tiles = board.tiles
+    .filter((tile) => tile.value !== 0)
+    .map((tile, index) => {
+      return <Tile tile={tile} />;
+    });
+
   return (
     <div>
-      {row.map((col, colIndex) => {
-      return <Cell />
-    })}
+      <div className="board">
+        {tiles}
+        {cells}
+      </div>
     </div>
   );
-});
-
-return <div>{cells}</div>
 };
 
 export default BoardView;

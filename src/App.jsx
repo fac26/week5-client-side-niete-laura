e5001d7bce886dfc9f02167a8a62939c665e9c4b
Niete <nieteis@gmail.com>
2023-02-15 11:41:37 +0000

    Made a bunch of refactoring changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,17 @@ import "./App.css";
 import BoardView from "./components/Board";
 
 function App() {
-  return <BoardView />;
+  return (
+    <div>
+      <h1>2048</h1>
+      <p>
+        Welcome to 2048! You can play the game using the arrow keys on your
+        keyboard. The aim of the game is to combine numbers of the same value
+        until you reach 2048.
+      </p>
+      <BoardView />
+    </div>
+  );
 }
 
 export default App;

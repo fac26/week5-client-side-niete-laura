6d7540f233eef4cfd75e009d2dd4acca91bed84c
LauraK0 <laurakeating@live.co.uk>
2023-02-15 10:22:32 +0000

    game over function added

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,35 +1,62 @@
-import React, { useState } from "react";
+import {useState} from "react";
 import Tile from "./Tile";
-import Cell from "./Cell";
-import { Board } from "../helper/index.js";
+import Cell from  "./Cell";
+import { Board } from "./game-logic.js"
+import useEvent from "../hooks/useEvent";
+import GameOverlay from "./GameOverlay";
 
 const BoardView = () => {
   const [board, setBoard] = useState(new Board());
 
+  const handleKeyDown = (event) => {
+    if (board.hasWon()) {
+      return;
+    } 
+    if (event.keyCode>=37 && event.keyCode<=40) {
+      // event.preventDefault();
+      let direction = event.keyCode - 37;
+      let boardClone = Object.assign(Object.create(Object.getPrototypeOf(board)), board);
+      let newBoard = boardClone.move(direction);
+      setBoard(newBoard);
+    }
+  }
+
+  useEvent("keydown", handleKeyDown);
+
   const cells = board.cells.map((row, rowIndex) => {
-    return (
-      <div>
-        {row.map((col, colIndex) => {
-          return <Cell />;
-        })}
-      </div>
-    );
-  });
-
-  const tiles = board.tiles
-    .filter((tile) => tile.value !== 0)
-    .map((tile, index) => {
-      return <Tile tile={tile} />;
-    });
-
   return (
-    <div>
-      <div className="board">
-        {tiles}
-        {cells}
-      </div>
+    <div key={rowIndex}>
+      {row.map((col, colIndex) => {
+      return <Cell key={rowIndex * board.size + colIndex} />
+    })}
     </div>
   );
-};
+});
+
+const tiles = board.tiles.filter((tile) => tile.value !== 0).map((tile, index) => {
+  return <Tile tile={tile} key={index}/>;
+})
+
+const resetGame = () => {
+  setBoard(new Board());
+}
+
+return (
+  <div> 
+    <div className="details-box">
+      <div className="resetButton" onClick={resetGame}>new game</div>
+      <div className="score-box">
+        <div className="score-header">SCORE</div>
+        <div>{board.score}</div>
+      </div>
+    </div>
+    <div className="board">
+    {board.hasLost() && <GameOverlay onRestart={resetGame} board={board} />}
+      {cells}
+      {tiles}
+    </div>
+  </div>
+  );
+}; 
 
 export default BoardView;
